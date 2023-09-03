52220d757900b40ab718120216f79b7aa6d5c8d0
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-09-02 22:39:54 -0700

    wip index with chessboard

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,33 @@
+<script setup>
+useSeoMeta({
+  title: 'Gambit Reborn',
+  description: 'Play Chess with friends online!',
+
+  ogTitle: 'Gambit Reborn',
+  ogDescription: 'Play Chess with friends online!',
+  ogImage: '/favicon.ico',
+  ogUrl: '[og:url]',
+
+  twitterTitle: 'Gambit Reborn',
+  twitterDescription: 'Play Chess with friends online!',
+  twitterImage: '/favicon.ico',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: { lang: 'en' },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.ico'
+    }
+  ]
+})
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
---
+++ nuxt-app/components/ChessBoard.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+const chessBoard = reactive(
+  [
+    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
+  ]
+)
+
+</script>
+
+<template>
+  <div>
+    <div v-for="rank in chessBoard">
+      <span v-for="square in rank">
+        {{ square == ' ' ? '&nbsp;' : square }}
+      </span>
+    </div>
+  </div>
+</template>

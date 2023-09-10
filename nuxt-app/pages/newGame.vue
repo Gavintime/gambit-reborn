06b016ea0ac8773d02440231e004a6dabc21c998
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-09-10 14:40:28 -0700

    Remove unused invite code

--- nuxt-app/pages/newGame.vue
+++ nuxt-app/pages/newGame.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-
-const gameCode = await useFetch('/api/newGame')
-
 </script>
 
 <template>
   <div>
     <ChessBoard />
-    <p>Invite Code: {{ gameCode.data.value?.code }}</p>
   </div>
 </template>

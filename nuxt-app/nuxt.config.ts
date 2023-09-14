c3e4ba4429427adfd2454cac1c8fa7de4534bf07
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-09-13 21:47:04 -0700

    add sanitize.css

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['sanitize.css', 'sanitize.css/forms.css', 'sanitize.css/typography.css'],
   devtools: { enabled: true },
   typescript: {
     // using takeover mode from volar extension

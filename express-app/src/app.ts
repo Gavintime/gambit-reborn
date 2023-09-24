26ae2eef378c385486a6bd25032ceb52452d95f2
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-09-23 21:32:01 -0700

    fix lint issues mostly semicolons

--- express-app/src/app.ts
+++ express-app/src/app.ts
@@ -1,20 +1,20 @@
-import express from 'express';
-import logger from 'morgan';
+import express from 'express'
+import logger from 'morgan'
 import compression from 'compression'
 
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
+import indexRouter from './routes/index.js'
+import usersRouter from './routes/users.js'
 
-const app = express();
+const app = express()
 
 // global middleware
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(compression());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(compression())
 
 // routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-export default app;
+export default app
--- express-app/src/routes/index.ts
+++ express-app/src/routes/index.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import { Router } from 'express'
 
-const router = Router();
+const router = Router()
 
 router.get('/', (req, res, next) => {
-  res.send({ endpoint: 'your at home' });
-});
+  res.send({ endpoint: 'your at home' })
+})
 
-export default router;
+export default router

3de81280de719c5f24f01ca724dbffdcc1d80bfa
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-09-23 17:28:53 -0700

    use esmodules + let

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+let router = Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({endpoint: 'your at home'});
+router.get('/', function (req, res, next) {
+  res.send({ endpoint: 'your at home' });
 });
 
-module.exports = router;
+export default router;

a36d0e075e48d2b10baacee68d6d9d0647077720
Gavintime <16593223+Gavintime@users.noreply.github.com>
2024-03-05 20:58:35 -0800

    get/post user

--- express-app/src/routes/user.ts
+++ express-app/src/routes/user.ts
@@ -4,7 +4,17 @@ import prisma from "../lib/prisma.js";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const user = await prisma.user.findFirst({ where: { id: 1 } });
+  const user = await prisma.user.findFirst({
+    where: { id: req.body.id },
+  });
+
+  res.json(user);
+});
+
+router.post("/", async (req, res) => {
+  const user = await prisma.user.create({
+    data: { name: req.body.name },
+  });
 
   res.json(user);
 });

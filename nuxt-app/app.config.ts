b80596d0dffb95ec37e37e84228a6ef3150e6497
Gavintime <16593223+Gavintime@users.noreply.github.com>
2024-03-05 20:47:02 -0800

    readd nuxt-app (for now)

---
+++ nuxt-app/app.config.ts
@@ -0,0 +1,5 @@
+export default defineAppConfig({
+  title: 'Gambit Reborn',
+  description: 'Play Chess with friends online!',
+  icon: '/favicon.ico'
+})
---
+++ nuxt-app/app.vue
@@ -0,0 +1,36 @@
+<script setup>
+const appConfig = useAppConfig()
+
+useSeoMeta({
+  title: appConfig.title,
+  description: appConfig.description,
+
+  ogTitle: appConfig.title,
+  ogDescription: appConfig.description,
+  ogImage: appConfig.icon,
+  ogUrl: '[og:url]',
+
+  twitterTitle: appConfig.title,
+  twitterDescription: appConfig.description,
+  twitterImage: appConfig.icon,
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: { lang: 'en' },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: appConfig.icon
+    }
+  ]
+})
+</script>
+
+<template>
+  <div>
+    <NavBar />
+    <NuxtPage />
+  </div>
+</template>

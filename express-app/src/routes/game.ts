085569dccc5348784241c53e44e67a704173905a
Gavintime <16593223+Gavintime@users.noreply.github.com>
2024-06-30 16:01:39 -0700

    flesh out get game endpoint

--- express-app/src/routes/game.ts
+++ express-app/src/routes/game.ts
@@ -1,22 +1,41 @@
 import { Router } from "express";
 import { Chess } from "chess.js";
+import Debug from "debug";
 import prisma from "../lib/prisma.js";
 
+const debug = Debug("gambit:game-route");
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const game = await prisma.game.findMany({
-    where: {
-      white: { id: 1 },
-    },
-    take: 2,
-  });
+router.get("/:id(\\d+)", async (req, res) => {
+  const { id } = req.params;
+  const idInt = parseInt(id, 10);
+  if (idInt > Number.MAX_SAFE_INTEGER) {
+    res.status(400).json("Invalid Game id");
+    return;
+  }
+
+  let game;
+  try {
+    game = await prisma.game.findUnique({
+      where: {
+        id: idInt,
+      },
+    });
+  } catch (error) {
+    res.status(500).json("Internal Error");
+    debug(error);
+    return;
+  }
+
+  if (game === null) {
+    res.status(404).json(`Game: ${id} doesn't exist.`);
+    return;
+  }
 
   res.json(game);
 });
 
 router.post("/", async (req, res) => {
-
   const game = await prisma.game.create({
     data: {
       plyCount: req.body.plyCount,
@@ -37,11 +56,11 @@ router.post("/test", async (req, res) => {
     chess.move(randomMove);
   }
 
-  const history = chess.history().join(' ');
+  const history = chess.history().join(" ");
 
   res.json({
-    "pgnString": history,
-    "pgnStringLength": history.length
+    pgnString: history,
+    pgnStringLength: history.length,
   });
 });
 

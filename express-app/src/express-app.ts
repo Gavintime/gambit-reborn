94e69877dd3142648926edb304c44e9f83bea962
Gavintime <16593223+Gavintime@users.noreply.github.com>
2024-08-24 12:52:39 -0700

    async error handling and custom 500 error handler

--- express-app/src/express-app.ts
+++ express-app/src/express-app.ts
@@ -1,27 +1,41 @@
 import express from "express";
 import logger from "morgan";
 import compression from "compression";
+import Debug from "debug";
+
+// handle async errors as if they were sync, calls next() when an async error happens
+// this prevents needing an async wrapper, or try catch boiler plate
+// !!!must be before router imports!!!
+import "express-async-errors";
 
 import userRouter from "./routes/UserRouter.js";
 import gameRouter from "./routes/GameRouter.js";
-import inviteCodeRouter from "./routes/InviteCodeRouter.js"
+import inviteCodeRouter from "./routes/InviteCodeRouter.js";
+
+const debug = Debug("gambit:express-app");
 
 const app = express();
 
 // global middleware
 app.use(logger("dev"));
-// TODO: handle invalid json in request, rn is sends a stacktrace to the user... 
+// TODO: handle invalid json in request, rn is sends a stacktrace to the user...
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 
 // routers
 app.use("/user", userRouter);
-app.use("/invite-code", inviteCodeRouter)
+app.use("/invite-code", inviteCodeRouter);
 app.use("/game", gameRouter);
 
 app.use((req, res) => {
   res.status(404).json("Endpoint not found");
+});
+
+app.use((err, req, res, next) => {
+  // TODO: better logging
+  debug(err);
+  res.status(500).json("Unknown Error");
 });
 
 export default app;
